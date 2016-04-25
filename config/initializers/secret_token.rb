d4657935a7118c3e321a1f359642857d1d41f46c
Farruh <ufarruh@gmail.com>
2016-04-25 20:24:13 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UStagram::Application.config.secret_key_base = 'bfde4a39665b6a8fa9214556579ed4c1ae9fa210259fe6a4666bdbb45c0ae8a3afe19ca9d869110d56f0ad5affa5458b6db8c364c9c5e96e182c440214ead231'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UStagram::Application.config.session_store :cookie_store, key: '_uStagram_session'
