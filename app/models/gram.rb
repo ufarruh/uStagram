9f7f56ad42e2fb9aea25aa1c64372cddcd9e9c8a
Farruh <ufarruh@gmail.com>
2016-04-27 21:37:34 +0000

    Connect user to grams

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,3 +1,4 @@
 class Gram < ActiveRecord::Base
   validates :message, presence: true
+  belongs_to :user
 end
--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
-
   describe "grams#index action" do
     it "should successfully show the page" do
       get :index
@@ -9,27 +8,62 @@ RSpec.describe GramsController, type: :controller do
     end
   end
 
+
   describe "grams#new action" do
+    it "should require users to be logged in" do
+      get :new
+      expect(response).to redirect_to new_user_session_path
+    end
     it "should successfully show the new form" do
+      user = User.create(
+        email:                 'fakeuser@gmail.com',
+        password:              'secretPassword',
+        password_confirmation: 'secretPassword'
+      )
+      sign_in user
+
       get :new
       expect(response).to have_http_status(:success)
     end
   end
 
-    describe "grams#create action" do
-      it "should successfully create a new gram in our database" do
-        post :create, gram: {message: 'Hello!'}
-        expect(response).to redirect_to root_path
 
-        gram = Gram.last
-        expect(gram.message).to eq("Hello!")
-      end
+  describe "grams#create action" do
+
+    it "should require users to be logged in" do
+    post :create, gram: { message: "Hello" }
+    expect(response).to redirect_to new_user_session_path
+  end
+
+    it "should successfully create a new gram in our database" do
+      user = User.create(
+        email:                 'fakeuser@gmail.com',
+        password:              'secretPassword',
+        password_confirmation: 'secretPassword'
+      )
+      sign_in user
+
+      post :create, gram: { message: 'Hello!'}
+      expect(response).to redirect_to root_path
+
+      gram = Gram.last
+      expect(gram.message).to eq("Hello!")
+      expect(gram.user).to eq(user)
     end
 
+    it "should properly deal with validation errors" do
+      user = User.create(
+        email:                 'fakeuser@gmail.com',
+        password:              'secretPassword',
+        password_confirmation: 'secretPassword'
+      )
+      sign_in user
 
-  it "should properly deal with validation errors" do
-    post :create, gram: {message: ''}
-    expect(response).to have_http_status(:unprocessable_entity)
-    expect(Gram.count).to eq 0
+      gram_count = Gram.count
+      post :create, gram: {message: '' }
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(gram_count).to eq Gram.count
+    end
+
   end
 end
