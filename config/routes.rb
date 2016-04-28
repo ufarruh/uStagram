e2ed6671df145c99e435b3952e8bc29e728a8d94
Farruh <ufarruh@gmail.com>
2016-04-28 00:23:56 +0000

    Add edit and uppdate posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ UStagram::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
   root "grams#index"
 
-  resources :grams, only: [:new, :create, :show]
+  resources :grams, only: [:new, :create, :show, :edit, :update]
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,12 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
-  describe "grams#edit" do
-    it "should successfully show the edit form if the gram is found" do
+
+  describe "grams#update" do
+    it "should allow users to successfully update grams" do
+      p = FactoryGirl.create(:gram, message: "Initial Value")
+      patch :update, id: p.id, gram: {message: "Changed"}
+      expect(response).to redirect_to root_path
+      p.reload
+      expect(p.message).to eq "Changed"
+    end
+
+    it "should have http 404 error if the gram cannot be found" do
+      patch :update, id: "YOLOSWAG", gram: {message: "Changed"}
+      expect(response).to have_http_status(:not_found)
+    end
+
+    it "should render the edit form with an http status of unprocessable_entity" do
+      p = FactoryGirl.create(:gram, message: "Initial Value")
+      patch :update, id: p.id, gram: { message: '' }
+      expect(response).to have_http_status(:unprocessable_entity)
+      p.reload
+      expect(p.message).to eq "Initial Value"
+    end
 
   end
 
+  describe "grams#edit" do
+    it "should successfully show the edit form if the gram is found" do
+      gram = FactoryGirl.create(:gram)
+      get :edit, id: gram.id
+      expect(response).to have_http_status(:success)
+  end
+
   it "should return a 404 error message if the gram is not found" do
+    get :edit, id: 'SWAG'
+    expect(response).to have_http_status(:not_found)
   end
 end
 
