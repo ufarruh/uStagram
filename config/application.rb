2382597a6083bbf656138a32c960dc365625b128
Farruh <ufarruh@gmail.com>
2016-05-02 17:12:31 +0000

    Chaneg application.rb

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
-config.assets.initialize_on_precompile = false
 
 module UStagram
   class Application < Rails::Application
