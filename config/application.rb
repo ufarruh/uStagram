bb940ff5c166e5e27f232b6222418418a5653b68
Farruh <ufarruh@gmail.com>
2016-05-02 16:02:14 +0000

    Change to application.rb to fix bug

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,11 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
+config.assets.initialize_on_precompile = false
 
 module UStagram
   class Application < Rails::Application
